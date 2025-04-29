97864cdf724e4392ad507acd5ab1c8ca77308164
dangkhoado43 <dangkhoado43@gmail.com>
2025-04-29 22:39:42 +0700

    [GDW-02] [FE] Create Banner for Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
+    const isSellerPath = useLocation().pathname.includes("seller");
+
     return (
         <div>
-            <Navbar />
+            {isSellerPath ? null : <Navbar />}
+
+            <div
+                className={`${
+                    isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32 py-4"
+                }`}
+            >
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    {/* <Route path="/products" element={<Products />} />
+                    <Route path="/product/:id" element={<Product />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/my-orders" element={<MyOrders />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} /> */}
+                </Routes>
+            </div>
         </div>
     );
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/images/assets";
-import { Menu, Search, ShoppingCart, User } from "lucide-react";
-import { useAppContext } from "../context/AppContext";
+import { Menu, Search, ShoppingCart } from "lucide-react";
+import { useAppContext } from "../contexts/AppContext";
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
@@ -15,15 +15,14 @@ const Navbar = () => {
 
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
-            <NavLink to="/">
+            <NavLink to="/" onClick={() => setOpen(false)}>
                 <img className="h-9" src={assets.logo} alt="logo" />
             </NavLink>
 
-            {/* Desktop Menu */}
             <div className="hidden sm:flex items-center gap-8">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/products">Products</NavLink>
-                <NavLink to="/">Contact</NavLink>
+                <NavLink to="/" className="hover:text-indigo-500">Home</NavLink>
+                <NavLink to="/products" className="hover:text-indigo-500">Products</NavLink>
+                <NavLink to="/" className="hover:text-indigo-500">Contact</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
                     <input
@@ -34,18 +33,14 @@ const Navbar = () => {
                     <Search />
                 </div>
 
-                <div className="relative cursor-pointer">
+                <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
                     <ShoppingCart />
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-indigo-500 w-[18px] h-[18px] rounded-full">
                         3
                     </button>
                 </div>
 
-                {!user ? (
-                    <button className="cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
-                        Login
-                    </button>
-                ) : (
+                {user ? (
                     <div className="relative group">
                         <img
                             src={assets.profile_icon}
@@ -53,10 +48,18 @@ const Navbar = () => {
                             alt="avatar"
                         />
                         <ul className="hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-3 w-30 rounded-md text-sm z-40">
-                            <li className="p-1.5 pl-3 hover:bg-gray-100 cursor-pointer">My Orders</li>
-                            <li className="p-1.5 pl-3 hover:bg-gray-100 cursor-pointer">Logout</li>
+                            <li onClick={() => navigate("/my-orders")} className="p-1.5 pl-3 hover:bg-gray-100 cursor-pointer">
+                                My Orders
+                            </li>
+                            <li onClick={() => handleLogout()} className="p-1.5 pl-3 hover:bg-gray-100 cursor-pointer">
+                                Logout
+                            </li>
                         </ul>
                     </div>
+                ) : (
+                    <button className="cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
+                        Login
+                    </button>
                 )}
             </div>
 
@@ -73,6 +76,20 @@ const Navbar = () => {
                     open ? "flex" : "hidden"
                 } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-3 px-5 text-sm md:hidden`}
             >
+                {user ? (
+                    <>
+                        <div className="flex items-center gap-2 cursor-pointer">
+                            <img
+                                src={assets.profile_icon}
+                                className="w-8"
+                                alt="avatar"
+                            />
+                            <p>User</p>
+                        </div>
+                    </>
+                ) : (
+                    <></>
+                )}
                 <NavLink to="/" onClick={() => setOpen(false)}>
                     Home
                 </NavLink>
@@ -90,15 +107,17 @@ const Navbar = () => {
                     Contact
                 </NavLink>
                 {user ? (
-                    <button
-                        onClick={() => {
-                            setOpen(false);
-                            handleLogout();
-                        }}
-                        className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm"
-                    >
-                        Logout
-                    </button>
+                    <>
+                        <button
+                            onClick={() => {
+                                setOpen(false);
+                                handleLogout();
+                            }}
+                            className="cursor-pointer px-6 py-2 mt-2 bg-red-500 hover:bg-red-600 transition text-white rounded-full text-sm"
+                        >
+                            Logout
+                        </button>
+                    </>
                 ) : (
                     <button
                         onClick={() => {
